4bfe7e8ffe0358777a663580185fc55968b8c3d4
Jeff Martin <jeff@reportmill.com>
2018-11-19 12:16:03 -0600

    ChartAreaPie: First stab at labels

--- src/rmcharts/app/ChartAreaPie.java
+++ src/rmcharts/app/ChartAreaPie.java
@@ -1,17 +1,25 @@
 package rmcharts.app;
+import java.text.DecimalFormat;
 import snap.gfx.*;
 
 /**
  * A ChartArea subclass to display the contents of pie chart.
  */
 public class ChartAreaPie extends ChartArea {
+    
+    // Whether legend was showing
+    boolean      _showLegend;
+    
+    // The format
+    DecimalFormat _fmt = new DecimalFormat("#.# %");
 
 /**
  * Creates a ChartAreaPie.
  */
 public ChartAreaPie()
 {
-    setPadding(25,10,10,10);
+    setPadding(25,10,20,10);
+    setFont(Font.Arial12.getBold());
 }
 
 /**
@@ -22,22 +30,52 @@ protected void paintChart(Painter aPntr, double aX, double aY, double aW, double
     // Get series
     DataSeries series = getSeries(0);
     int pointCount = getValueCount();
+    Font font = getFont(); aPntr.setFont(font);
     
     double cw = getWidth(), ch = getHeight();
     Insets ins = getInsetsAll();
-    double diam = ch - ins.getHeight();
+    double diam = ch - ins.getHeight(), rad = diam/2;
     double px = ins.left + Math.round((cw - ins.getWidth() - diam)/2);
     double py = ins.top + Math.round((ch - ins.getHeight() - diam)/2);
     
     double vals[] = new double[pointCount]; for(int i=0;i<pointCount;i++) vals[i] = series.getValue(i);
     double total = 0; for(int i=0;i<pointCount;i++) total += vals[i];
-    double angles[] = new double[pointCount]; for(int i=0;i<pointCount;i++) angles[i] = Math.round(vals[i]/total*360);
-    double start = -90;
+    double reveal = getReveal(), max = 360*reveal;
+    double angles[] = new double[pointCount]; for(int i=0;i<pointCount;i++) angles[i] = Math.round(vals[i]/total*max);
+    double start = -90, selIndex = _chartView._toolTipView.getValueIndex();
     
+    // Iterate over angles and paint wedges
     for(int i=0; i<angles.length; i++) { double angle = angles[i];
-        Arc arc = new Arc(px - (i>0?10:0), py - (i>0?10:0), diam, diam, start, angle);
+    
+        // Get offset point (adjusted if wedge selected)
+        double px2 = px, py2 = py; if(i==selIndex) {
+            double ang2 = Math.toRadians(start + angle/2);
+            px2 = px + 10*Math.cos(ang2); py2 = py + 10*Math.sin(ang2);
+        }
+        
+        // Get diameter (adjusted, along with offsets, for reveal)
+        double diam2 = diam; if(reveal<1) { diam2 = diam2*reveal; px2 += rad*(1-reveal); py2 += rad*(1-reveal); }
+        
+        // Create arc and fill
+        Arc arc = new Arc(px2, py2, diam2, diam2, start, angle);
         aPntr.setColor(_chartView.getColor(i));
-        aPntr.fill(arc); start += angle;
+        aPntr.fill(arc);
+        
+        // Paint label
+        String name = series.getPoint(i).getName();
+        if(reveal>=1 && name!=null && name.length()>0) {
+            String label = name + ": " + _fmt.format(vals[i]/total);
+            double ang2 = start + angle/2, ang2Rad = Math.toRadians(ang2);
+            double px3 = px2 + rad + (rad+20)*Math.cos(ang2Rad);
+            double py3 = py2 + rad + (rad+20)*Math.sin(ang2Rad) + font.getAscent();
+            Rect bnds = font.getStringBounds(label);
+            if(ang2>90) px3 -= bnds.width;
+            px3 = Math.round(px3); py3 = Math.round(py3);
+            aPntr.setColor(Color.BLACK); aPntr.drawString(label, px3, py3);
+        }
+        
+        // Increment start
+        start += angle;
     }
 }
 
@@ -79,6 +117,7 @@ public void activate()
 {
     ChartXAxis xaxis = _chartView.getXAxis(); xaxis.setVisible(false); xaxis.setManaged(false);
     ChartYAxis yaxis = _chartView.getYAxis(); yaxis.setVisible(false); yaxis.setManaged(false);
+    _showLegend = _chartView.isShowLegend(); _chartView.setShowLegend(false);
 }
 
 /**
@@ -88,6 +127,7 @@ public void deactivate()
 {
     ChartXAxis xaxis = _chartView.getXAxis(); xaxis.setVisible(true); xaxis.setManaged(true);
     ChartYAxis yaxis = _chartView.getYAxis(); yaxis.setVisible(true); yaxis.setManaged(true);
+    _chartView.setShowLegend(_showLegend);
 }
 
 }
